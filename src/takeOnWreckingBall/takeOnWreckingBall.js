257f29ed4ee72478388ba0018a7eadb9b2dbf245
Andreas Stassivik <andreas@stassi.net>
2018-10-15 16:08:35 -0700

    +renderText

--- src/takeOnWreckingBall/takeOnWreckingBall.js
+++ src/takeOnWreckingBall/takeOnWreckingBall.js
@@ -1,29 +1,30 @@
 import distribution from './distribution';
 import markovianSentences from '../markovianSentences';
 
-const separatedNewlines = x => x.join('\n');
-const separatedDoubleNewlines = x => x.join('\n\n');
+const renderText = separator => collection => collection.join(separator);
+const renderNewlines = renderText('\n');
+const renderDoubleNewlines = renderText('\n\n');
 
 const bars = markovianSentences(distribution);
 const bar = bars();
 const bars2 = bars(2);
 const bars4 = bars(4);
 
-const takeOnWreckingBall = (seeds = []) => separatedDoubleNewlines([
+const takeOnWreckingBall = (seeds = []) => renderDoubleNewlines([
   bars4(seeds[0]),
   bars4(seeds[1]),
-  separatedNewlines([
+  renderNewlines([
     bars4(seeds[2]),
     bar(seeds[3]),
   ]),
   bars4(seeds[4]),
   bars4(seeds[1]),
   bars4(seeds[2]),
-  separatedNewlines([
+  renderNewlines([
     bars4(seeds[5]),
     bar(seeds[3]),
   ]),
-  separatedNewlines([
+  renderNewlines([
     bars2(seeds[6]),
     bar(seeds[7]),
     bar(seeds[8]),
@@ -32,7 +33,7 @@ const takeOnWreckingBall = (seeds = []) => separatedDoubleNewlines([
   ]),
   bars2(seeds[1]),
   bars4(seeds[2]),
-  separatedNewlines([
+  renderNewlines([
     bars4(seeds[5]),
     bar(seeds[3]),
   ]),
