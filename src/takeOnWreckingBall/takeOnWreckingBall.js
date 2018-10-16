558d3ac677c569056f7ae8134f2648bf7835d1c5
Andreas Stassivik <andreas@stassi.net>
2018-10-15 22:45:03 -0700

    4-step transform, partial application

--- src/takeOnWreckingBall/takeOnWreckingBall.js
+++ src/takeOnWreckingBall/takeOnWreckingBall.js
@@ -1,43 +1,114 @@
+import {
+  join,
+  juxt,
+  lensIndex,
+  pipe,
+  view,
+} from 'ramda';
 import distribution from './distribution';
 import markovianSentences from '../markovianSentences';
 
-const renderText = separator => collection => collection.join(separator);
-const renderNewlines = renderText('\n');
-const renderDoubleNewlines = renderText('\n\n');
+const mergeNewlines = join('\n');
+const mergeDoubleNewlines = join('\n\n');
 
+const viewIndex = pipe(lensIndex, view);
 const bars = markovianSentences(distribution);
-const bar = bars();
-const bars2 = bars(2);
-const bars4 = bars(4);
+const indicesToBars = barCount => index => pipe(
+  viewIndex(index),
+  bars(barCount),
+);
 
-const takeOnWreckingBall = (seeds = []) => renderDoubleNewlines([
-  bars4(seeds[0]),
-  bars4(seeds[1]),
-  renderNewlines([
-    bars4(seeds[2]),
-    bar(seeds[3]),
-  ]),
-  bars4(seeds[4]),
-  bars4(seeds[1]),
-  bars4(seeds[2]),
-  renderNewlines([
-    bars4(seeds[5]),
-    bar(seeds[3]),
-  ]),
-  renderNewlines([
-    bars2(seeds[6]),
-    bar(seeds[7]),
-    bar(seeds[8]),
-    bars2(seeds[6]),
-    bar(seeds[8]),
-  ]),
-  bars2(seeds[1]),
-  bars4(seeds[2]),
-  renderNewlines([
-    bars4(seeds[5]),
-    bar(seeds[3]),
-  ]),
-  bar(seeds[3]),
+const toBar = indicesToBars();
+const to2Bars = indicesToBars(2);
+const to4Bars = indicesToBars(4);
+
+const seedsToBars = juxt([
+  toBar(3),
+  toBar(7),
+  toBar(8),
+  to2Bars(1),
+  to2Bars(6),
+  to4Bars(0),
+  to4Bars(1),
+  to4Bars(2),
+  to4Bars(4),
+  to4Bars(5),
 ]);
+
+const indicesSixAndSeven = [
+  viewIndex(6),
+  viewIndex(7),
+];
+
+const indicesNineAndZero = [
+  viewIndex(9),
+  viewIndex(0),
+];
+
+const arrangeBars = juxt([
+  viewIndex(5),
+  ...indicesSixAndSeven,
+  viewIndex(0),
+  viewIndex(8),
+  ...indicesSixAndSeven,
+  ...indicesNineAndZero,
+  viewIndex(4),
+  viewIndex(1),
+  viewIndex(2),
+  viewIndex(4),
+  viewIndex(2),
+  viewIndex(3),
+  viewIndex(7),
+  ...indicesNineAndZero,
+  viewIndex(0),
+]);
+
+const mergeLines = ([
+  a,
+  b,
+  c,
+  d,
+  e,
+  f,
+  g,
+  h,
+  i,
+  j,
+  k,
+  l,
+  m,
+  n,
+  o,
+  p,
+  q,
+  r,
+  ...props
+]) => [
+  a,
+  b,
+  mergeNewlines([c, d]),
+  e,
+  f,
+  g,
+  mergeNewlines([h, i]),
+  mergeNewlines([
+    j,
+    k,
+    l,
+    m,
+    n,
+  ]),
+  o,
+  p,
+  mergeNewlines([q, r]),
+  ...props,
+];
+
+const takeOnWreckingBall = pipe(
+  seedsToBars,
+  arrangeBars,
+  mergeLines,
+  mergeDoubleNewlines,
+);
 
 export default takeOnWreckingBall;
